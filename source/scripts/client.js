04abe1cb23b40c67cd1a9659cee5fcd3ed427990
vadi4 <vadich92@gmail.com>
2019-06-12 12:07:24 +0300

    new

---
+++ source/scripts/client.js
@@ -0,0 +1,31 @@
+$(function() {
+	$('body').on('click', '.js-burger-link', function(e) {
+		e.preventDefault();
+		var $this = $(this);
+		var $dropdownWrap = $('.b-header-nav__dropdown-wrap');
+
+		if( $this.hasClass('js-act') ) {
+			$this.removeClass('js-act');
+			$dropdownWrap.removeClass('js-act');
+		} else {
+			$this.addClass('js-act');
+			$dropdownWrap.addClass('js-act');
+		}
+	});
+
+	$(document).mouseup(function (e){ 
+		var div = $(".b-header-nav__dropdown-wrap, .js-burger-link");
+		if (!div.is(e.target)
+		    && div.has(e.target).length === 0) {
+			$('.js-burger-link, .b-header-nav__dropdown-wrap').removeClass('js-act');
+		}
+	});	
+});
+
+$(window).on('scroll', function() {
+	if( $(window).scrollTop() > 0 ) {
+		$('.l-header').addClass('js-fixed');
+	} else {
+		$('.l-header').removeClass('js-fixed');
+	}
+});
